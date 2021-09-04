64fea67a5e469a353338b6cb01269326ce881ffb
P-Raj27 <85934814+P-Raj27@users.noreply.github.com>
2021-09-04 10:57:06 +0530

    Create PROBLEM-2.py

---
+++ PROBLEM-2.py
@@ -0,0 +1,18 @@
+# Cybrilla_Assignment
+This Repo contains the code for the Cybrilla Assignment
+list1 = []
+
+list1 = [int(item) for item in input("Enter the list items : ").split()]
+
+flag = False
+for i in range(len(list1)-1):
+  x = set()
+  for k in range(i+1, len(list1)):
+    summ = -(list1[i]+list1[k])
+    if summ in x:
+      print(summ, list1[i], list1[k])
+      flag = True
+    else:
+      x.add(list1[k])
+if flag == False:
+  print("not found")
